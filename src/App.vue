254366040113ee49612786b1dafc2c7f95b2a582
stephenhofstad <stephenhofstad@gmail.com>
2020-05-13 11:48:31 -0500

    added login, singup, and logout components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <a href="/signup">Signup</a>|
+      <a href="/login">Login</a>|
+      <a href="/logout">Logout</a>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
-<template>
+ <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>All Contests</h1>
+    <div v-for="contest in contests">
+      <h2>{{ contest.date }}</h2>
+      <p>Cutoff: {{ contest.time }}</p>
+      <p>Win: {{ contest.win }}</p>
+      <p>Temp: {{ contest.line }}</p>
+    </div>
   </div>
 </template>
+ <style>
+</style>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require("axios");
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      contests: [],
+    };
+  },
+  created: function() {
+    axios.get("/api/contests").then(response => {
+      this.contests = response.data;
+    });
+  },
+  methods: {},
+};
 </script>
